1c853d50b10d36520a1cd7d23f19470e05d35d0a
Tori Machen <tmachen@pillartechnology.com>
2018-12-26 11:18:51 -0600

    added probably not valuable test around displaying title to practice test writing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('App', () => {
+    it('renders the form title', () => {
+        const wrapper = shallow(<App/>);
+        const title = wrapper.find('.form-title').text();
+
+        expect(title).toEqual('React FizzBuzz Form');
+    });
 });
